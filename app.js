6d6c668ef7f5fc8c59565e6610df510753ce5637
Afolabi Afeez <folabidare005@gmail.com>
2026-05-17 11:35:16 +0100

    Fixed MongoDB connection and server setup

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-require ("dotenv").config();
+require("dotenv").config();
 
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
+require("dotenv").config();
 
-console.log(process.env.MONGO_URL);
+const PORT = process.env.PORT
+
+const app = express()
+
+app.use(express.json())
+
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
-    console.log("Connected to MongoDB");
+  console.log("MongoDB connected");
 })
 .catch((err) => {
-    console.log(err);
+  console.log(err);
 });
 
-//middleware
-app.use(express.json());
-app.use(helmet());
-app.use(morgan("common"));
+app.get("/", (req, res) => {
+  res.send("Welcome Home!")
+})
 
 
-
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log("Backend server is running!");
 });
